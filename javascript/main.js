30ad499d597af4b46ce327622ecf10699dcedf59
emirbojadzija <emir.bojadzija@hotmail.com>
2024-02-05 20:32:04 +0100

    Autokuca projekat

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+
+var countDownDate = new Date("Sep 20, 2021 16:00:00").getTime();
+
+
+ var x = setInterval(function() {
+
+  
+  var now = new Date().getTime();
+    
+  
+  var distance = countDownDate - now;
+    
+  
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  
+  document.getElementById("Odbrojavanje").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+   
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Odbrojavanje").innerHTML = "Cijena artikla je istekla!";
+  }
+}, 1000);
